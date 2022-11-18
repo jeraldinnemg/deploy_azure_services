1b60ab6167c4298a9774a88df2b7f13ced294eed
Jeraldinne Molleda <71736942+jeraldinnemg@users.noreply.github.com>
2022-11-18 15:02:04 -0300

    Create ScriptOperaciones.ps1

---
+++ ExampleScripts/ScriptOperaciones.ps1
@@ -0,0 +1,19 @@
+# Operaciones Básicas
+
+$num1=10
+$num2=2
+
+"datos:"
+$num1
+$num2
+
+$suma=$num1 + $num2
+$resta=$num1 - $num2
+$producto=$num1 * $num2
+$division=$num1 / $num2
+
+
+Write-Output "suma: $suma"
+Write-Output "resta: $resta"
+Write-Output "producto: $producto"
+Write-Output "division: $division"
