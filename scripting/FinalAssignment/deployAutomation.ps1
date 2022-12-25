2e86a2df7660884d268d9b3c209c72be9ba90121
Jeraldinne Molleda <71736942+jeraldinnemg@users.noreply.github.com>
2022-12-25 15:11:55 -0300

    working in ARM templates and global script

--- scripting/FinalAssignment/deployAutomation.ps1
+++ scripting/FinalAssignment/deployAutomation.ps1
@@ -1,59 +1,41 @@
-#Define parameters of the RG
 param(
+
     [Parameter(Mandatory)]$rgName,
-    [Parameter(Mandatory)]$location
+    $location = "eastus",
+    [Parameter(Mandatory)]$storageAccountName
+
 )
 
-#Connecto to Azure Account
 Connect-AzAccount
+
     Write-Output "####################"
     Write-Output "Getting Resource Details"
     Write-Output "####################"
 
-    #Validate if the name given is available
+
+
     $existingRG = Get-AzResourceGroup | Where-Object {$_.ResourceGroupName -eq $rgName}
 
-    #Validate if the name given is according to the EY naming convention
-    function Validate-AzureResourceName
-    {
-      # Get the na
-      [CmdletBinding()]
-      param (
-        [Parameter(Mandatory=$true, Position=0)]
-        [string]$rgName
-      )
-    
-      # Regular expression(regex) to validate the naming convention:
-      # <Cloud provider 2 chars>-<Location 3 chars>-<Environment 1 char>-<DeploymentID 6 chars>-<Resource Type 3 chars>-<Sequence 2 chars>
-      # Valid name example: az-eus-d-001122-rg-01 / Invalid name example: az-eus-d-001122-vm-001
-      [regex]$rx = ('^[a-zA-Z]{2}-[a-zA-Z]{3}-[a-zA-Z]{1}-[a-zA-Z0-9]{6}-[a-zA-Z0-9]{3}-[a-zA-Z0-9]{2}$')
 
-    }
-    
-
-    #If there isn't a resource group with the name given, create the RG
 if(!$existingRG){
     Write-Output "####################"
     Write-Output "Creating Resource Group"
     Write-Output "####################"
-    
-    try {
-        #Define the resourceGroup in a variable
-        Validate-AzureResourceName -Name $rgName
-         # Validation of the name given with regex
-        if ($regex.IsMatch($rGName))
-        {
-            $newRg= New-AzResourceGroup -Name $rgName -Location $location
 
-            #Create a resource group with the RG variable as a parameter, and deploy the ARM template of the resource "desired"
-            New-AzResourceGroupDeployment -ResourceGroupName $newRG.ResourceGroupName -TemplateFile "./storageDeploy.json"
-        }
-       
+    try {
+        New-AzResourceGroup -Name $rgName -Location $location
     }
     catch {
         Throw "Deployment failed: $_"
     }
+    
 }
+
 else{
-    New-AzResourceGroupDeployment -ResourceGroupName $existingRG.ResourceGroupName -TemplateFile "./storageDeploy.json"
+
+    $hashtableParameters = @{
+        storageAccountName = $storageAccountName
+    }
+
+    New-AzResourceGroupDeployment -ResourceGroupName $existingRG.ResourceGroupName -TemplateFile "./storageDeploy.json" -TemplateParameterObject $hashtableParameters
 }
